06c4a5252af8abba14485ffd6abfdc8795b3d2e6
killeraliens <killeraliens@outlook.com>
2019-11-26 09:56:44 -0700

    bookmarks seed

---
+++ seeds/seed.bookmarks.sql
@@ -0,0 +1,7 @@
+INSERT INTO bookmarks
+  (title, url, description, rating)
+VALUES
+  ('uuid generator seed', 'https://www.uuidgenerator.net/version1', 'generates uuids', 3),
+  ('goat''s guide seed', 'http://www.goatsguide.com', 'metal', 5),
+  ('agalloch seed', 'https://www.youtube.com/watch?v=JE_JnhgLnpw', 'youtube video', 5),
+  ('evoken seed', 'https://kreation-records.bandcamp.com/album/shades-of-night-descending-kr-38', 'bcammp evoken', 4);
